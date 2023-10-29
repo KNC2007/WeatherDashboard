263cc5af854c44ba5f974af67e9c0eb40769f0a0
KNC2007 <cnkalbach@gmail.com>
2023-10-29 16:18:25 -0400

    fixing appending items to page

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,38 @@
 var requestUrlCurrentWeather = 'https://api.openweathermap.org/data/2.5/weather?q=';
 var ApiKey = '&appid=ba33442054f9de507b0968f397619ed5&units=imperial';
-var searchCityButton = $('.btn');
+var searchCityButton = $('#searchBtn');
 var searchInput = $('#city-input');
 var currentCityHeader = $('.currentWeatherCity');
 var currentWeatherConditions = $('ul');
 
 
-
+// searchInput.Value
 
 
 
 function getApi() {
-    fetch(requestUrlCurrentWeather + searchInput.value + ApiKey)
+    fetch(requestUrlCurrentWeather + 'Tampa' + ApiKey)
     .then(function (response) {
         return response.json();
     })
     .then(function (data){
         console.log(data);
-        for (let i = 0; i < data.length; index++) {
-            var createCityHeader = document.createElement('h3');
-            createCityHeader.text(data[i].name);
-            currentCityHeader.appendChild(createCityHeader);
+        // for (let i = 0; i < data.length; index++) {
+            $(currentCityHeader).text("Currently in: " + data.name);
+            // currentCityHeader.appendChild(createCityHeader);
 
-            var listItemCurrent = $('li').text(Math.floor(data[i].main.temp) + "°F");
-            currentWeatherConditions.appendChild(listItemCurrent);
+            $('ul').append(Math.floor(data.main.temp) + "°F");
+            // currentWeatherConditions.appendChild(listItemCurrentTemp);
 
-            
-            
+            $('ul').append(Math.floor(data.wind.speed) + " MPH");
+            // currentWeatherConditions.appendChild(listItemCurrentWind);
+
+            $('ul').append(data.main.humidity + "%");
+            // currentWeatherConditions.appendChild(listItemCurrentHumidity);
         }
-
-    }
-    
+    // }
     )
 }
 
-searchCityButton.addEventListener('click', getApi());
+getApi()
+// $(searchCityButton).onclick = getApi;
