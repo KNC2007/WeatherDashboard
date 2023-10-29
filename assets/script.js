dc8fb9ffa4ac12609a3b7b586c4ac2b1a4133f2b
KNC2007 <cnkalbach@gmail.com>
2023-10-29 15:11:35 -0400

    fetch api and creating elements

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,29 @@
-var requestUrlCurrentWeather = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=ba33442054f9de507b0968f397619ed5';
-var searchCityButton = 
-var tableBody = document.getElementById('repo-table');
+var requestUrlCurrentWeather = 'https://api.openweathermap.org/data/2.5/weather?q=';
+var ApiKey = '&appid=ba33442054f9de507b0968f397619ed5&units=imperial';
+var searchCityButton = $('.btn');
+var searchInput = $('#city-input');
+var currentCityHeader = $('.currentWeatherCity');
+var currentWeatherConditions = $('ul');
+
 
 
 
 
 
 function getApi() {
-    fetch(requestUrlCurrentWeather)
+    fetch(requestUrlCurrentWeather + searchInput.value + ApiKey)
     .then(function (response) {
-     return response.json()   ;
+        return response.json();
     })
     .then(function (data){
-        console.log(data)
+        console.log(data);
         for (let i = 0; i < data.length; index++) {
             var createCityHeader = document.createElement('h3');
-            createCityHeader.appendChild()
+            createCityHeader.text(data[i].name);
+            currentCityHeader.appendChild(createCityHeader);
+
+            var listItemCurrent = $('li').text(Math.floor(data[i].main.temp) + "°F");
+            currentWeatherConditions.appendChild(listItemCurrent);
 
             
             
@@ -25,3 +33,5 @@ function getApi() {
     
     )
 }
+
+searchCityButton.addEventListener('click', getApi());
